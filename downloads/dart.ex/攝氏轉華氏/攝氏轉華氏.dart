8da47addb92773a80736162e31f280ff68a97fa8
40823104 <40823104@gm.nfu.edu.tw>
2020-01-07 19:17:09 +0800

    按鈕

--- downloads/dart.ex/攝氏轉華氏/攝氏轉華氏.dart
+++ downloads/dart.ex/攝氏轉華氏/攝氏轉華氏.dart
@@ -1,7 +1,17 @@
-# 說明此 yaml 檔案所引用程式的目的
-name: use Dart to implement Runge-Kutta Method
-# 引用模式為純 dart 或 html 模式
-mode: dart
-# 所包含引入的程式名稱, 若為純 dart 則只包含 dart 程式, 若為 html 模式, 則加入 index.htm 與 style.css
-files:
-  - name: runge_kutta.dart
+﻿int x = 0;
+num F = 86;
+num C = 30;
+
+void main(){
+if (x == 0) {
+num F2 = C*9/5+32;
+print('F=$F2');
+  }
+  else{
+    
+  num C2 = (F-32)*5/9;
+	print('C=$C2');
+  }
+  print('x=0,1切換(華氏、攝氏)');
+  
+}
